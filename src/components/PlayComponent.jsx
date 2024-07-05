f0eda5f5d6428267eba50c3f471fe9d2a53dce94
MarkMallari16 <mallariftw01@gmail.com>
2024-07-05 19:06:05 +0800

    fix some ui

--- src/components/PlayComponent.jsx
+++ src/components/PlayComponent.jsx
@@ -6,12 +6,12 @@ const PlayComponent = () => {
     const { handleIsPlaying } = usePlay();
 
     return (
-        <div className='h-screen flex items-center justify-center'>
+        <div className='h-screen flex items-center justify-center p-5 '>
             <div className='w-full max-w-2xl'>
                 <div className='flex justify-center items-center gap-4 mb-4'>
                     <div>
-                        <h1 className=' text-4xl font-black'>Quiz Game</h1>
-                        <p>Created by Mark Mallari</p>
+                        <h1 className=' text-7xl lg:text-6xl font-black'>Quiz Game</h1>
+                        <p className='text-center'>Created by Mark Mallari</p>
                     </div>
                 </div>
                 <div className='text-center'>
