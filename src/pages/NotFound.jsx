c14fa694c089b086e76ae72f93778769ae620e43
MarkMallari16 <mallariftw01@gmail.com>
2024-07-05 19:11:51 +0800

    not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import usePlay from '../hooks/usePlay'
 
 const NotFound = () => {
+  const { handleExit } = usePlay();
   return (
-    <div>Pages not Found!</div>
+    <div className='h-screen flex  justify-center items-center'>
+      <div className='text-center'>
+        <h1 className='text-6xl font-black mb-4'>404 Not Found!</h1>
+        <button className='btn btn-primary w-full' onClick={handleExit}>Go back to main page</button>
+      </div>
+    </div>
   )
 }
 
